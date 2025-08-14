7538385f92918039647a4989e04f9c68342a29c1
Snehil <snehilsuthar81@gmail.com>
2025-08-14 16:20:17 +0530

    change1

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express');
-//sdjfadjl'H;H;FJ;FJ;JA;F;KJ;FJ
 const mongoose = require('mongoose');
 const app = express();
-//aaasdfsa
 const mongoConnect = require('./src/database/connection')
 const authRoute= require('./src/routes/userSignUpRoute')
 const userRoute = require('./src/routes/userDetailsRoute')
 app.use(express.json())
 require('dotenv').config()
 mongoConnect(process.env.MONGO_URL).then(() => console.log('Mongodb Connected!')).catch((err) => console.log(`Error while Connecting MongoDb , Error:${err}`))
-
+//line no 10 par change
 app.listen(process.env.PORT,() => console.log('app started '))
 
 app.use('/auth',authRoute)
